9c055d5b4a0954af95ea0e738f8725dac68d4c33
meenaH <meena.haq@gmail.com>
2017-10-15 19:25:29 +0100

    Delete

--- snippets/mapArray.js
+++ snippets/mapArray.js
@@ -1,16 +1,1 @@
-for (var x = 1; x <=12; x++){
-	console.log(x,'times table')
-	for (var y = 1; y<=12; y++){
-		console.log('\t%s * %s = %s', x, y, x * y)
-	}
 
-}
-
-	var numbers = [1,2,3,4];
-	numbers.map(function(a) {
-		console.log(a, 'times table');
-	numbers.map(function(b){
-		console.log('\t', a, '*', b, '=', a * b)
-	});
-	
-}):
